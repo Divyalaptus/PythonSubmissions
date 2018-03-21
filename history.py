f9ed1e3a73368ea6166319e57fd6952c99dd95d2
Divyalaptus <divyanshk555@gmail.com>
2018-03-20 17:55:08 -0700

    day1  introduction of SPY!!!

---
+++ history.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sat Feb 10 16:03:24 2018)---
+runfile('C:/Users/Divyalaptus/.spyder-py3/temp.py', wdir='C:/Users/Divyalaptus/.spyder-py3')
+
+## ---(Tue Mar 20 16:34:23 2018)---
+runfile('C:/Users/Divyalaptus/.spyder-py3/temp.py', wdir='C:/Users/Divyalaptus/.spyder-py3')
+print("how are you")
+runfile('C:/Users/Divyalaptus/.spyder-py3/temp.py', wdir='C:/Users/Divyalaptus/.spyder-py3')
+print(status)
+runfile('C:/Users/Divyalaptus/.spyder-py3/temp.py', wdir='C:/Users/Divyalaptus/.spyder-py3')
---
+++ projectFile.py
@@ -0,0 +1,10 @@
+print("let's get started")
+
+
+####################### ( Get spy details ) ########################
+
+spy_name = input("What is your name?\n")
+print("Welcome!!! "+spy_name+". Glad to have you back with us.")
+spy_salutation = input("What shall we call you (Mr. or Mrs.?)")
+spy_name = spy_salutation+" "+spy_name
+spy_status = input("Alright "+spy_name+". I'd like to know a little bit more about...")
